ea9f23d052861543d32cb2631d1877d7b83de44a
9huit <nantenkf@gmail.com>
2025-02-08 00:15:08 +0300

    Update plat_finie.gd

--- plats/plat_finie.gd
+++ plats/plat_finie.gd
@@ -1,17 +1,24 @@
 extends Node2D  # (ou StaticBody2D selon ton cas)
-
+var gobelin_present: Goblin = null  # Stocke le gobelin s'il est dans la zone
 var id_plat = null  # ID du plat cuisiné
 
 func _ready():
 	if id_plat != null:
 		print("Plat prêt à être ramassé, ID :", id_plat)
 
-func _on_body_entered(body):
-	if body is Goblin:  # Vérifie si le gobelin est à proximité
-		body.recuperer_plat_cuit(self)  # Appelle la fonction de ramassage
-
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Goblin:  # Vérifie si le gobelin est à proximité
-		body.recuperer_plat_cuit(self)  # Appelle la fonction de ramassage
-		print("a proxi")
+	if body is Goblin:
+		gobelin_present = body  # Stocke le gobelin
+		print("Gobelin à proximité")
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Goblin:
+		gobelin_present = null  # Supprime la référence si le gobelin part
+		print("Gobelin est parti")
+
+func _process(_delta):
+	if gobelin_present and Input.is_action_just_pressed("grab"):
+		gobelin_present.recuperer_plat_cuit(self)
+		print("Le gobelin a récupéré le plat")
+		#queue_free()  # Supprime le plat après récupération
